674c8cd26297965741e56d173be21a4ba82366c1
matthew <mbrockman4@huskers.unl.edu>
2025-08-28 12:29:23 -0500

    Clean up for Comments

--- CurrencyConverter.c
+++ CurrencyConverter.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
 int main(void) {
-    float dollars=1.0;
+    //Var definition
+    float dollars,toGbp,toJpy,Fee;
+
+    //Ask for amount input
     printf("Please input the total amount of US Dollars:");
     scanf("%f",&dollars);
-    float helper = dollars/10;
+
+    //Calulate fee amount
+    Fee = dollars/10;
+
+    //Print Fee amount
     printf("Fee (10%): ");
-    printf("%f\n",helper);
+    printf("%f\n",Fee);
+
+    //Convert 1/2 of dollars after tax into jap and pound
+    toGbp = ((dollars-(dollars/10))/2)*0.79;
+    toJpy = ((dollars-(dollars/10))/2)*127.65;
+
+
+    //Output after conversion
     printf("You get:\n");
-    float contogbp = ((dollars-(dollars/10))/2)*0.79;
-    float contojpy = ((dollars-(dollars/10))/2)*127.65;
-    printf("%f GBP\n",contogbp);
-    printf("%f JPY",contojpy);
+    printf("%f GBP\n",toGbp);
+    printf("%f JPY",toJpy);
 
     return 0;
 }
